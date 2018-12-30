f325d39f3ac3c393863c87bd9ad7b69576d648c3
LJEsp <esparragoleandroj@gmail.com>
2018-12-30 13:36:40 +0800

    Publish to gh pages

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.493d253b99fa84ee4e0a0120d0c39512.js"
+  "/project-ui-challenges/precache-manifest.ca0e817d4779aeffe16b6114bedc9fce.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/project-ui-challenges/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
