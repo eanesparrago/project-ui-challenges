a90cbf4ee502ef518921c9f6feb4ca2de98e8d22
LJEsp <esparragoleandroj@gmail.com>
2019-01-08 19:50:44 +0800

    Comment

--- src/006_elecy/Elecy.js
+++ src/006_elecy/Elecy.js
@@ -87,6 +87,7 @@ const StyledElecy = animated(styled.div`
     flex-wrap: wrap;
   }
 
+  /* >>> This is for desktop */
   @media (min-height: 600px) and (min-width: 600px) {
     width: 320px;
     height: 528px;
